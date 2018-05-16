9e326aaa0c1eb8a12656aa930710de9e03ee21ab
Laura Unger <laura.unger@hotmail.de>
2018-05-16 21:00:52 +0200

    Added CrossOrigin to LocationController for CORS, added some additional data

--- backend/hebebuehnen/src/main/java/de/thm/eps/hebebuehnen/rest/LocationController.java
+++ backend/hebebuehnen/src/main/java/de/thm/eps/hebebuehnen/rest/LocationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +17,7 @@ public class LocationController {
 	@Autowired
 	private LocationService locationService;
 	
+	@CrossOrigin
 	@RequestMapping(value="/locations")
 	public @ResponseBody List<Location> search(@RequestParam(name = "city", required=false, defaultValue="") String city) {
 		
--- backend/hebebuehnen/src/main/resources/data.sql
+++ backend/hebebuehnen/src/main/resources/data.sql
@@ -5,17 +5,24 @@ insert into location (id, city, description) values
 (4, 'Hamburg', 'Hebebuehnen Place'),
 (5, 'Muenchen', 'Hebebuehnen einfach leihen'),
 (6, 'Bruchkoebel', 'Zentrale'),
-(7, 'Dortmund', 'Hebebuehnen Mueller');
+(7, 'Dortmund', 'Hebebuehnen Mueller'),
+(8, 'Stuttgart', 'Hebebuehnen Wilhelma'),
+(9, 'Dresden', 'Hebebuehnen am Zoo'),
+(10, 'Kiel', 'Hebebuehnen Lauterbach');
 
 insert into hebebuehne (id,type,name,location_id) values 
 (1, 'small','buehne_small1',6),
 (2, 'small','buehne_small2',6),
 (3, 'small','buehne_small3',6),
-(4, 'medium','buehne_medium1',1),
-(5, 'medium','buehne_medium2',4),
-(6, 'medium','buehne_medium3',5),
-(7, 'big','buehne_big1',1),
-(8, 'big','buehne_big2',2),
-(9, 'big','buehne_big3',3),
-(10, 'small','buehne_small4',1),
-(11, 'small','buehne_small5',7);
+(4, 'small','buehne_small4',1),
+(5, 'small','buehne_small5',7),
+(6, 'medium','buehne_medium1',1),
+(7, 'medium','buehne_medium2',4),
+(8, 'medium','buehne_medium3',5),
+(9, 'medium','buehne_medium4',8),
+(10, 'big','buehne_big1',1),
+(11, 'big','buehne_big2',2),
+(12, 'big','buehne_big3',3),
+(13, 'big','buehne_big4',9),
+(14, 'big','buehne_big5',10);
+
